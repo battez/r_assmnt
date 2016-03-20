23e51c55b0a46cbb7110f0c1453405e3fbf089cc
luke barker <j.l.p.barker@dundee.ac.uk>
2016-03-20 21:27:54 +0000

    shiny added

--- app1/server.R
+++ app1/server.R
@@ -1,5 +1,10 @@
 library(shiny)
 library(datasets)
+aq <-  read.table("../airquality.csv", header=TRUE, 
+                       sep=",")
+df <- aq[complete.cases(aq), ]
+
+df$Full <- factor(df$Month, levels = 1:12, labels = month.name)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -11,27 +16,42 @@ shinyServer(function(input, output) {
   #     when inputs change
   #  2) Its output type is a plot
   
-  # Return the requested dataset
-  datasetInput <- reactive({
-    switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
+
+  
+  output$mainPlot <- renderPlot({
+   
+    x <- input$scatter
+    plot(df[, x], df$Ozone, main = paste("Plot: Ozone and", x), ylab = 'Ozone', xlab = x)
+    
+    
+   if(input$toggle == TRUE)
+   {
+     model <- lm(df$Ozone ~ df[ ,x])
+     abline(model, lwd = 1, col="blue")
+   }
+
+    
+    
   })
   
   output$distPlot <- renderPlot({
-     x    <- faithful[, 2]  # Old Faithful Geyser data
+     
+     x    <-   df[, input$column]
      bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
    # draw the histogram with the specified number of bins
-   hist(x, breaks = bins, col = 'red', border = 'white')
+   hist(x, breaks = bins, col = 'red', main=paste('Histogram of', input$column))
+   
   })
   
   output$summary <- renderPrint({
-    summary(datasetInput())
+    print(paste('Summary statistics displayed for: ', input$column));
+    summary(df[, input$column]);
+    
+    
   })
   
   output$table <- renderTable({
-    data.frame(x=datasetInput())
+    # data.frame(x=datasetInput())
   })
 })
--- app1/ui.R
+++ app1/ui.R
@@ -3,33 +3,50 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(
   fluidPage(
-  
+  # my css
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
   # Application title
-  titlePanel("Programming for Data - R assignment"),
-  
+  headerPanel("R Programming - Air Quality data in Shiny"),
+  titlePanel("by Luke Barker, MSc. Data Engineering 2015/16."),
+ 
+ 
   # Sidebar with a slider input for the number of bins
   sidebarLayout(position = "right",
     sidebarPanel(
+      width=5,
+      radioButtons("scatter", "Plot - relationship between Ozone and:",
+                   list("Solar.R","Wind", "Temp"),
+                   selected ="Wind"),
+      checkboxInput("toggle", 
+                    label = "Toggle regression fit ", value = FALSE),
+      hr(),
+      selectInput("column", 
+                  label = "Show Summary / Histogram for:",
+                  choices = list("Ozone", "Solar.R","Wind", "Temp"),
+                  selected = "Ozone"),
       
-      selectInput("dataset", "Choose a dataset:", 
-                  choices = c("rock", "pressure", "cars")),
       sliderInput("bins",
-                  "Bob:",
+                  "Pick no. of histogram bins:",
                   min = 2,
-                  max = 30,
-                  value = 30),
-      img(src='http://g-ecx.images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg')
+                  max = 25,
+                  value = 15),
+      
+      
+      img(src='http://capita.wustl.edu/capita/capitareports/GlobeO3/goodbad1.gif', width="100%")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
+      width=7,
       tabsetPanel(type = "tabs", 
-                  tabPanel("Plot", plotOutput("distplot")), 
-                  tabPanel("Summary", verbatimTextOutput("summary")), 
-                  tabPanel("Table", tableOutput("table"))
+          tabPanel("Plot", plotOutput("mainPlot")), 
+          tabPanel("Summary Stats", verbatimTextOutput("summary")), 
+          tabPanel("Histogram", plotOutput("distPlot"))
                   
       
-    )
+      )
   )
   )
 ))
